139f04e0f1b3de950b02cd65980dafa08b73c591
talaltag <talal@theappguys.com>
2025-01-23 23:23:03 +0000

    Assistant checkpoint: Removed middleware and simplified _app.tsx
    Assistant generated file changes:
    - middleware.ts: Remove middleware file contents
    - pages/_app.tsx: Simplify _app.tsx
    
    ---
    
    User prompt:
    
    remove the middleware for now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,9 @@
 import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import type { AppProps } from 'next/app';
-import { Provider } from 'react-redux';
-import { store } from '../store/store';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
