2762b2d111fead8bed373e3e736ea1d840e4c074
talaltag <talal@theappguys.com>
2025-02-04 10:45:37 +0000

    Assistant checkpoint: Update leaderboard layout and positioning
    Assistant generated file changes:
    - pages/user/dashboard.tsx: Center first player
    
    ---
    
    User prompt:
    
    Make the Leaderboard text and More text outside the yellow background color and
    also make sure to make the 1st player in center of both 2nd player and 3rd player
    first image is required design
    second image is your design

--- pages/user/dashboard.tsx
+++ pages/user/dashboard.tsx
@@ -268,7 +268,7 @@ export default function UserDashboard() {
                   <div className="d-flex justify-content-around align-items-end mb-4">
                     <div
                       className="text-center order-1"
-                      style={{ marginTop: "80px", marginRight: "-30px" }}
+                      style={{ marginTop: "80px" }}
                     >
                       <div className="position-relative">
                         <Image
@@ -305,7 +305,7 @@ export default function UserDashboard() {
                         1223
                       </div>
                     </div>
-                    <div className="text-center order-0" style={{ marginTop: "20px" }}>
+                    <div className="text-center order-0" style={{ marginTop: "20px", marginLeft: "40px", marginRight: "40px" }}>
                       <div className="position-relative">
                         <Image
                           src="/user1.png"
@@ -338,7 +338,7 @@ export default function UserDashboard() {
                     </div>
                     <div
                       className="text-center order-2"
-                      style={{ marginTop: "80px", marginLeft: "-30px" }}
+                      style={{ marginTop: "80px" }}
                     >
                       <div className="position-relative">
                         <Image
