c9fe8a5f0661f03e685a7cbc776c2c317c93fde7
talal17 <38963407-talal17@users.noreply.replit.com>
2025-02-10 06:46:45 +0000

    User checkpoint: Update notification styling to match provided image.

--- pages/user/settings.tsx
+++ pages/user/settings.tsx
@@ -127,7 +127,7 @@ export default function Settings() {
         </div>
         <p className="text-muted">Manage your team and preferences here.</p>
 
-        <Nav className="mb-4 border-0 d-flex gap-1" style={{ padding: '4px', borderRadius: '6px' }}>
+        <Nav className="mb-4 d-flex gap-1" style={{ width:'fit-content', border:'1px solid #ECECEC !important', padding: '4px', borderRadius: '6px' }}>
           <NavItem>
             <NavLink
               className={classnames("border-0 rounded", {
